22b1a238a811a460c292de48beb1ef1f81743e66
Syed Faiz <faizmiya110@gmail.com>
2023-07-19 10:18:13 +0530

    invoice

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -72,7 +72,18 @@
 
                 </ul>
             </li><!-- End Components Nav -->
-
+            <li class="nav-item">
+                <a class="nav-link collapsed" data-bs-target="#form" data-bs-toggle="collapse" href="#">
+                    <i class="bi bi-calendar4-event"></i><span>Form</span><i class="bi bi-chevron-down ms-auto"></i>
+                </a>
+                <ul id="form" class="nav-content collapse " data-bs-parent="#sidebar-nav">
+                    <li>
+                        <a href="{{ route('create.invoice') }}">
+                            <i class="bi bi-circle"></i><span>Invoice</span>
+                        </a>
+                    </li>
+                </ul>
+            </li><!-- End Components Nav -->
 
 
         </ul>
